ba28fb69c51ff1b82b6f3954d5b9adfa539c0df2
Matthew Francis-Landau <matthew@matthewfl.com>
2010-10-25 21:34:42 -0700

    forgot to change the header back to style.css

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <html>
   <head>
     <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
-    <!--link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css"-->
-    <link rel="stylesheet/less" href="<?php bloginfo('template_url'); ?>/style.less" type="text/css">
-    <script src="<?php bloginfo('template_url'); ?>/less.js" ></script>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css">
+    <!--link rel="stylesheet/less" href="<?php bloginfo('template_url'); ?>/style.less" type="text/css">
+    <script src="<?php bloginfo('template_url'); ?>/less.js" ></script-->
     <script src="<?php bloginfo('template_url'); ?>/jquery.js" ></script>
     <script src="<?php bloginfo('template_url'); ?>/base.js" ></script>
   </head>
